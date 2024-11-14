cf8bd3ccb0f51dd01dc7b411dbc2bea2275a249f
kanha-9770 <app3.nessco@gmail.com>
2024-11-14 07:11:29 +0530

    second commit

--- src/components/Navbar/nav-menue.tsx
+++ src/components/Navbar/nav-menue.tsx
@@ -79,11 +79,11 @@ const MenuItem = ({
   const componentCodeourCompany = pathname.split("/")[3]?.toLowerCase();
   const pathSegments = pathname?.split('/') || [];
   // Assuming URL structure like /<countryCode>/<languageCode>
-  const countryCode = pathSegments[0] || '';
-  const languageCode = pathSegments[1] || '';
+  const countryCode = pathSegments[1] || '';
+  const languageCode = pathSegments[2] || '';
   return (
     <div ref={ref} className="z-10 cursor-pointer px-3 font-poppins">
-      <Link className="invert-0 text-base font-light" href={`${countryCode}/${languageCode}/${link}`}>
+      <Link className="invert-0 text-base font-light" href={`/${languageCode}/${link}`}>
         {item}
       </Link>
       {active === item && (
