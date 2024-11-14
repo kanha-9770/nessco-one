85ecffb4de1f99d2aec286ec2105be5a0b58805a
kanha-9770 <app3.nessco@gmail.com>
2024-11-14 06:41:26 +0530

    second commit

--- src/components/Navbar/nav-menue.tsx
+++ src/components/Navbar/nav-menue.tsx
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useCountryAndLanguage } from "@/global";
 
 export const Menu = ({ children }: { children: React.ReactNode }) => {
   const [active, setActive] = useState<string | null>(null);
@@ -77,10 +78,11 @@ const MenuItem = ({
   const pathname = usePathname() || "";
   const componentCode = pathname.split("/")[2]?.toLowerCase();
   const componentCodeourCompany = pathname.split("/")[3]?.toLowerCase();
+  const { countryCode, languageCode } = useCountryAndLanguage();
 
   return (
     <div ref={ref} className="z-10 cursor-pointer px-3 font-poppins">
-      <Link prefetch={true} className="invert-0 text-base font-light" href={`/${link}`}>
+      <Link prefetch={true} className="invert-0 text-base font-light" href={`${countryCode}/${languageCode}/${link}`}>
         {item}
       </Link>
       {active === item && (
