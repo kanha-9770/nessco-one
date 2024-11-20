fc62e2d2883b391a6739b276f516022327e6b138
kanha-9770 <app3.nessco@gmail.com>
2024-11-20 16:21:44 +0530

    added

--- src/components/product/Pages.tsx
+++ src/components/product/Pages.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Page1 from "@/components/product/Header";
-import dynamic from "next/dynamic";
 import { ProductItem } from "./types/constant";
-const Page2 = dynamic(() => import("@/components/product/ProductCatalouge"));
+// const Page2 = dynamic(() => import("@/components/product/ProductCatalouge"));
 
 interface ProductProps {
   productData: ProductItem;
@@ -12,7 +11,7 @@ const Pages: React.FC<ProductProps> = ({ productData }) => {
   return (
     <>
       <Page1 productData={productData} />
-      <Page2 productData={productData} />
+      {/* <Page2 productData={productData} /> */}
     </>
   );
 };
