425610a540d5f01ff7a2c647bc9b6ccb159d60b7
WebsdNessco <151986798+WebsdNessco@users.noreply.github.com>
2024-11-29 18:14:00 +0000

    productGallery mobile view change

--- src/components/applicationLayout/ProductGallery.tsx
+++ src/components/applicationLayout/ProductGallery.tsx
@@ -156,7 +156,7 @@ const Page3: React.FC<CombinedProps> = ({
                 {page3product?.image?.map((item, idx) => (
                   <div
                     key={idx}
-                    className={`bg-white lg:w-[22rem] w-auto h-40 lg:h-[22rem] lg:rounded-[1rem] rounded-[0.5rem] flex items-center justify-center overflow-hidden cursor-pointer ${
+                    className={`bg-white lg:w-[22rem] w-40 h-40 lg:h-[22rem] lg:rounded-[1rem] rounded-[0.5rem] flex items-center justify-center overflow-hidden cursor-pointer ${
                       selectedImage === item.img
                         ? "lg:border-none border-2 border-red-700"
                         : "border-none"
